0b82e7c09182df2f7470519644c9aeeedfdc9014
Stephen Wright <sjw948@gmail.com>
2017-03-23 20:37:14 +0000

    fixed prototype syntax

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,8 @@
 import React, { PropTypes } from "react";
 
 const Counter = (props) => {
-  const onChange = React.PropTypes.func.isRequired,
-  const score = React.PropTypes.number.isRequired,
+  const onChange = React.PropTypes.func.isRequired;
+  const score = React.PropTypes.number.isRequired;
   return (
    <div className="counter" >
      <button className="counter-action decrement" onClick={() => props.onChange(-1)}>
